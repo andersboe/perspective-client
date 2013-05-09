407741740afe6beb0649c227f9bd37c4f70878a0
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-05-09 17:20:42 +0200

    Added sections test on run function

--- test/js/modules/app/appSpec.js
+++ test/js/modules/app/appSpec.js
@@ -1,57 +1,62 @@
 define(function(require) {
 
-    var App = require('modules/app/app');
-    var Section = require('components/section');
+  var App = require('modules/app/app');
+  var Section = require('components/section');
 
-    describe('app', function() {
+  describe('app', function() {
 
-        var app, el;
+    var app, el;
 
-        beforeEach(function() {
-            el = $('<div></div>');
-            app = new App({ el: el });
+    beforeEach(function() {
+      el = $('<div></div>');
+      app = new App({ el: el });
+    });
+
+    afterEach(function() {
+      app.destroy();
+    });
+
+    describe('sections', function() {
+
+      it('are defined', function() {
+        app.addSections({
+          'content': '.content'
         });
 
-        afterEach(function() {
-            app.destroy();
+        expect(app.sections.content).toBeDefined();
+      });
+
+      it('are instances of section', function() {
+        app.addSections({
+          'content': '.content'
         });
 
-        describe('sections', function() {
-
-            it('are defined', function() {
-                app.addSections({
-                    'content': '.content'
-                });
-
-                expect(app.sections.content).toBeDefined();
-            });
-
-            it('are instances of section', function() {
-                app.addSections({
-                    'content': '.content'
-                });
-
-                expect(app.sections.content instanceof Section).toBe(true);
-            });
-
-        });
-
-        describe('run', function() {
-
-            it('renders the app layout', function() {
-                expect(app.$el.html()).toEqual('');
-                app.run(function() {});
-                expect(app.$el.html()).not.toEqual('');
-            });
-
-            it('triggers the done callback when the setup is finished', function() {
-                var spy = sinon.spy();
-                app.run(spy);
-                expect(spy).toHaveBeenCalledOnce();
-            });
-
-        });
+        expect(app.sections.content instanceof Section).toBe(true);
+      });
 
     });
+
+    describe('run', function() {
+
+      it('renders the app layout', function() {
+        expect(app.$el.html()).toEqual('');
+        app.run(function() {});
+        expect(app.$el.html()).not.toEqual('');
+      });
+
+      it('defines section', function() {
+        app.run(function() {});
+        expect(app.sections.main).toBeDefined();
+      });
+
+      it('triggers the done callback when the setup is finished', function() {
+        var spy = sinon.spy();
+        app.run(spy);
+        expect(spy).toHaveBeenCalledOnce();
+      });
+
+    });
+
+  });
 
 });
