edd403eb458eab7dfaed64ec22c5841dfc31c552
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-06-06 22:32:01 +0200

    Added som menu items

--- src/js/modules/menu/menuItemView.js
+++ src/js/modules/menu/menuItemView.js
@@ -2,13 +2,16 @@ define(function(require) {
 
   var View = require('base/view');
 
+  var menuItemTemplate = require('hb!./menuItem');
+
   return View.extend({
+
+    template: menuItemTemplate,
 
     tagName: 'li',
 
     render: function() {
       this.renderTemplate();
-
       return this;
     }
 
--- src/js/modules/menu/menuView.js
+++ src/js/modules/menu/menuView.js
@@ -2,17 +2,24 @@ define(function(require) {
 
   var View = require('base/view');
   var menuTemplate = require('hb!./menu');
+  var MenuItemView = require('./menuItemView');
 
   return View.extend({
 
     template: menuTemplate,
 
     initialize: function() {
+      this.listView = new MenuItemView();
+      this.boardView = new MenuItemView();
 
+      this.addSubView(this.listView);
+      this.addSubView(this.boardView);
     },
 
     render: function() {
       this.renderTemplate();
+
+      this.$('.menu-list').html([this.listView.render().el, this.boardView.render().el]);
 
       return this;
     }
