a1d40cd4cee3c5adbefca62589e0ad27300beb27
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-17 11:19:44 +0200

    Wrote a test for checking if view renders a h2 element

---
+++ src/js/not_found/notFoundSpec.js
@@ -0,0 +1,14 @@
+define(function(require) {
+
+  var NotFound = require('not_found/notFound');
+
+  describe('notFound', function() {
+    it('renders a h2', function () {
+      var $element = $('<div>');
+      var view = new NotFound({el: $element});
+
+      expect(view.fragment.html).toContain('h2');
+    });
+  });
+
+});
