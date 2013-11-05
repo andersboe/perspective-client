2dd790d48fe10f13868ac27d427e46a2cee56890
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-11-05 18:16:58 +0100

    Switch between production and dev mode

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
 SERVER_PORT=8000 JENKINS_URL="http://localhost:8888" JENKINS_WS_HREF="ws://localhost:8888" JENKINS_WS_PROTOCOL="perspective-protocol" TASKS_URL="http://localhost:8889" nodemon index.js
+#NODE_ENV="production" SERVER_PORT=8000 JENKINS_URL="http://localhost:8888" JENKINS_WS_HREF="ws://localhost:8888" JENKINS_WS_PROTOCOL="perspective-protocol" TASKS_URL="http://localhost:8889" nodemon index.js
