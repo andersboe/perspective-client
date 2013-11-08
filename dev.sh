0132055892abe1d9b5177f00ec579e9c0b06b597
Welcor <thomas.arp@gmail.com>
2013-11-08 13:51:59 +0000

    can now run on windows with cygwin and with node.js installed

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
-SERVER_PORT=8000 JENKINS_URL="http://localhost:8888" JENKINS_WS_HREF="ws://localhost:8888" JENKINS_WS_PROTOCOL="perspective-protocol" TASKS_URL="http://localhost:8889" nodemon index.js
+SERVER_PORT=8000 JENKINS_URL="http://localhost:8181" JENKINS_WS_HREF="ws://10.43.1.18:8888" JENKINS_WS_PROTOCOL="perspective-protocol" TASKS_URL="http://localhost:8889" nodemon index.js
 #NODE_ENV="production" SERVER_PORT=8000 JENKINS_URL="http://localhost:8888" JENKINS_WS_HREF="ws://localhost:8888" JENKINS_WS_PROTOCOL="perspective-protocol" TASKS_URL="http://localhost:8889" nodemon index.js
