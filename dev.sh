f5ee34ddd8c94130a223ae81ba2da1f98e8e017a
Torkil Vederhus <torkilv@gmail.com>
2014-06-03 14:36:57 +0200

    Update dev.sh
    index.js seems to have been renamed to server.js

--- dev.sh
+++ dev.sh
@@ -7,6 +7,6 @@ SERVER_PORT=8000 \
 EVENTS_URL="http://localhost:8890" \
 JENKINS_URL="http://localhost:8888" \
 TASKS_URL="http://localhost:8889" \
-nodemon index.js
+nodemon server.js
 
 #NODE_ENV="production" SERVER_PORT=8000 JENKINS_URL="http://localhost:8888" JENKINS_WS_HREF="ws://localhost:8888" JENKINS_WS_PROTOCOL="perspective-protocol" TASKS_URL="http://localhost:8889" nodemon index.js
